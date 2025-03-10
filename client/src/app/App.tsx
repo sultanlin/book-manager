d16a1777c803151c49ba3a2f219e8ea8e248e002
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-03-10 06:08:37 +0300

    improve project file structure

---
+++ client/src/app/App.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react"
+// import "./App.css"
+import "./style.css"
+import MainPage from "../features/home/MainPage"
+
+function App() {
+  const [data, setData] = useState("")
+  useEffect(() => {
+    fetch("http://192.168.1.44:8080/", {
+      method: "GET",
+      // mode: "cors",
+    })
+      .then((res) => res.json())
+      .then((res) => setData(res.message))
+      .catch((e) => console.log(e))
+  }, [])
+  console.log(data)
+
+  return (
+    <div>
+      <MainPage />
+    </div>
+  )
+}
+
+export default App
