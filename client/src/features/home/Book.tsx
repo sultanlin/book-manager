e4222c5ec312472687dfa85564b71bd73a764ef9
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-03-11 07:03:56 +0300

    improve book layout and showcase layers
    added css grid to wrap and layout the book showcase, changed html order
    for books, removed book cover fixed width to give grid control of cover
    size.

--- client/src/features/home/Book.tsx
+++ client/src/features/home/Book.tsx
@@ -2,15 +2,14 @@ import BookProps from "../../types/BookProps"
 
 function Book({ book }: { book: BookProps }) {
   return (
-    <article>
-      <p>Name: {book.name}</p>
-      <p>Author: {book.author}</p>
-      <p>Cover:</p>
+    <article className="book">
       <img
         src={book.cover}
         className="cover"
         alt="everything for dummies book cover"
       />
+      <p>Name: {book.name}</p>
+      <p>Author: {book.author}</p>
     </article>
   )
 }
--- client/src/features/home/Showcase.tsx
+++ client/src/features/home/Showcase.tsx
@@ -7,10 +7,27 @@ function Showcase(): ReactNode {
   const books: BookProps[] = [
     { name: "first book", cover: img, author: "J.K. 1" },
     { name: "second book", cover: img, author: "J.K. 2" },
+    { name: "first book", cover: img, author: "J.K. 1" },
+    { name: "first book", cover: img, author: "J.K. 1" },
+    { name: "first book", cover: img, author: "J.K. 1" },
+    { name: "first book", cover: img, author: "J.K. 1" },
+    { name: "first book", cover: img, author: "J.K. 1" },
+    { name: "first book", cover: img, author: "J.K. 1" },
+    { name: "first book", cover: img, author: "J.K. 1" },
+    { name: "first book", cover: img, author: "J.K. 1" },
+    { name: "first book", cover: img, author: "J.K. 1" },
+    { name: "second book", cover: img, author: "J.K. 2" },
+    { name: "second book", cover: img, author: "J.K. 2" },
+    { name: "second book", cover: img, author: "J.K. 2" },
+    { name: "second book", cover: img, author: "J.K. 2" },
+    { name: "second book", cover: img, author: "J.K. 2" },
+    { name: "second book", cover: img, author: "J.K. 2" },
+    { name: "second book", cover: img, author: "J.K. 2" },
+    { name: "second book", cover: img, author: "J.K. 2" },
+    { name: "second book", cover: img, author: "J.K. 2" },
   ]
   return (
     <main className="content">
-      <h1>Books</h1>
       {books.map((b) => (
         <Book book={b} />
       ))}
