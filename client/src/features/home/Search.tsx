9e10d0e6cd0d7c65a955d71a1a490ebbe3a9bc13
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-03-13 07:03:04 +0300

    add form, input and search icon for search bar

--- client/src/features/home/Search.tsx
+++ client/src/features/home/Search.tsx
@@ -1,5 +1,13 @@
+import SearchIcon from "@mui/icons-material/Search"
 function Search() {
-  return <div>Search</div>
+  return (
+    <form className="search">
+      <input type="text" name="search" id="search" placeholder="Search books" />
+      <label htmlFor="search">
+        <SearchIcon className="icon" />
+      </label>
+    </form>
+  )
 }
 
 export default Search
