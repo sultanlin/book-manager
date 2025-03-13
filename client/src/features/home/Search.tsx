3dbbe7a5cc477b32c3fdc719b67840fe1b8c7090
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-03-14 01:33:03 +0300

    add option to search for authors instead of books

--- client/src/features/home/Search.tsx
+++ client/src/features/home/Search.tsx
@@ -2,10 +2,21 @@ import SearchIcon from "@mui/icons-material/Search"
 function Search() {
   return (
     <form className="search">
-      <input type="text" name="search" id="search" placeholder="Search books" />
-      <label htmlFor="search">
-        <SearchIcon className="icon" />
-      </label>
+      <div className="searchbar">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search books or authors"
+        />
+        <label htmlFor="search">
+          <SearchIcon className="icon" />
+        </label>
+      </div>
+      <select>
+        <option>Book</option>
+        <option>Author</option>
+      </select>
     </form>
   )
 }
