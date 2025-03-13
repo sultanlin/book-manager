1aeddfd15fd947c62856984a518c7d90a2df1d21
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-03-13 06:16:33 +0300

    add logo html, style and layout

--- client/src/features/home/Logo.tsx
+++ client/src/features/home/Logo.tsx
@@ -1,5 +1,11 @@
+import AlignHorizontalLeftIcon from "@mui/icons-material/AlignHorizontalLeft"
 function Logo() {
-  return <div className="logo">My Library</div>
+  return (
+    <div className="logo">
+      <AlignHorizontalLeftIcon color="primary" sx={{ fontSize: 30 }} />
+      <h1>Bookshelf</h1>
+    </div>
+  )
 }
 
 export default Logo
