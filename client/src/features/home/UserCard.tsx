511ef4fd318b80e927a2429761ad1d590c8a5cb8
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-03-13 06:21:31 +0300

    add usercard component and style it with layout

---
+++ client/src/features/home/UserCard.tsx
@@ -0,0 +1,11 @@
+import AccountCircleIcon from "@mui/icons-material/AccountCircle"
+function UserCard() {
+  return (
+    <div className="usercard">
+      <h3>Sultan</h3>
+      <AccountCircleIcon />
+    </div>
+  )
+}
+
+export default UserCard
