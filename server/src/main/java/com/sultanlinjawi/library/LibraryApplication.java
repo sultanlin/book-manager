228b52e52b645578ac0a072accc90a2dadd9b609
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-03-18 07:59:57 +0300

    add basic graphql client for hardcover api

--- server/src/main/java/com/sultanlinjawi/library/LibraryApplication.java
+++ server/src/main/java/com/sultanlinjawi/library/LibraryApplication.java
@@ -1,10 +1,17 @@
 package com.sultanlinjawi.library;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.graphql.client.ClientGraphQlResponse;
+import org.springframework.graphql.client.ClientResponseField;
+import org.springframework.graphql.client.FieldAccessException;
+import org.springframework.graphql.client.HttpSyncGraphQlClient;
+import org.springframework.web.client.RestClient;
 
 import java.util.Arrays;
 
@@ -13,6 +20,54 @@ public class LibraryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryApplication.class, args);
+
+        // RestClient restClient = RestClient.create("https://api.hardcover.app/v1/graphql");
+
+        RestClient client =
+                RestClient.builder().baseUrl("https://api.hardcover.app/v1/graphql").build();
+        HttpSyncGraphQlClient graphQlClient =
+                HttpSyncGraphQlClient.builder(client)
+                        .headers(
+                                (headers) ->
+                                        headers.add(
+                                                "authorization", System.getenv("authorization")))
+                        .build();
+
+        String bookName = "dune";
+        JsonNode book =
+                graphQlClient
+                        .documentName("bookByName")
+                        .variable("bookName", bookName)
+                        .retrieveSync("search")
+                        .toEntity(JsonNode.class);
+
+        System.out.println(book);
+
+        String document =
+                """
+ {
+    search(query: "%s", query_type: "Book",
+    per_page: 10, page: 1) {
+        results
+    }
+}
+"""
+                        .formatted(bookName);
+
+        try {
+            JsonNode project =
+                    graphQlClient
+                            .document(document)
+                            .retrieveSync("search")
+                            .toEntity(JsonNode.class);
+            // System.out.println("Project is " + project);
+        } catch (FieldAccessException ex) {
+            ClientGraphQlResponse response = ex.getResponse();
+            // ...
+            ClientResponseField field = ex.getField();
+            // return fallback value
+            System.out.println("No book found?!?");
+        }
     }
 
     @Bean
