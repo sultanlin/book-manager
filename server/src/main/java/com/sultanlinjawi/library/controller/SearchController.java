c743a99d4774aa262f6c702208a967f1c504d0fa
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-03-25 07:14:59 +0300

    add API for searching books from hardcover and remove experiment code
    Make the search service to query hardcover and search controller at
    /search that uses the service. Delete code used to experiment with
    graphql queries in java and json deserializing.

---
+++ server/src/main/java/com/sultanlinjawi/library/controller/SearchController.java
@@ -0,0 +1,22 @@
+package com.sultanlinjawi.library.controller;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.sultanlinjawi.library.service.SearchService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SearchController {
+
+    private final SearchService searchService;
+
+    public SearchController(SearchService searchService) {
+        this.searchService = searchService;
+    }
+
+    @GetMapping("/search")
+    public JsonNode getResults() {
+        return searchService.searchBook();
+    }
+}
---
+++ server/src/main/java/com/sultanlinjawi/library/service/SearchService.java
@@ -0,0 +1,57 @@
+package com.sultanlinjawi.library.service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import org.springframework.graphql.client.ClientGraphQlResponse;
+import org.springframework.graphql.client.ClientResponseField;
+import org.springframework.graphql.client.FieldAccessException;
+import org.springframework.graphql.client.HttpSyncGraphQlClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
+public class SearchService {
+    public JsonNode searchBook() {
+        String bookName = "lord of the rings";
+
+        String document =
+                """
+query bookByName($bookName: String!) {
+    search(query: $bookName, query_type: "Book",
+    per_page: 1, page: 1) {
+        results
+    }
+}
+""";
+
+        RestClient restClient = RestClient.create("https://api.hardcover.app/v1/graphql");
+
+        HttpSyncGraphQlClient graphQlClient =
+                HttpSyncGraphQlClient.builder(restClient)
+                        .headers(
+                                (headers) ->
+                                        headers.add(
+                                                "authorization", System.getenv("authorization")))
+                        .build();
+
+        JsonNode project = null;
+        // TODO: Check out proper graphql query practices for error handling
+        try {
+            project =
+                    graphQlClient
+                            .document(document)
+                            .variable("bookName", bookName)
+                            .retrieveSync("search")
+                            .toEntity(JsonNode.class);
+        } catch (FieldAccessException ex) {
+            ClientGraphQlResponse response = ex.getResponse();
+            System.out.println(response);
+            // ...
+            ClientResponseField field = ex.getField();
+            System.out.println(field);
+            // return fallback value
+            System.out.println(ex);
+        }
+        return project;
+    }
+}
