322c7af1cb0c0b3e20e98724779b92cc41cf4900
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-03-27 16:51:58 +0300

    add search api, early stage

---
+++ client/src/api/search.ts
@@ -0,0 +1,18 @@
+// TODO: unmarshal json to appropriate obj
+// name, author, cover
+export default function searchBooks(
+  bookName: string,
+  queryType: string = "book",
+) {
+  let typeParam = ""
+  if (queryType) {
+    typeParam = `type=${queryType}`
+  }
+  const queryParams = `?name=${bookName}&${typeParam}`
+  console.log(queryParams)
+
+  fetch("http://localhost:8080/api/v1/search" + queryParams)
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err))
+}
