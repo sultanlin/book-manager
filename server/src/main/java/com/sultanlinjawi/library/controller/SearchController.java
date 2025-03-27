fc22968e138910a031b9d5f945e3114193be4226
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-03-27 16:50:47 +0300

    setup cors on java server

--- server/src/main/java/com/sultanlinjawi/library/controller/SearchController.java
+++ server/src/main/java/com/sultanlinjawi/library/controller/SearchController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin // (origins = "http://localhost:5173")
+@CrossOrigin
 @RestController
 public class SearchController {
 
