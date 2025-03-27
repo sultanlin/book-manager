e33262af09931f604b307bdae5e5a715c76f0f4c
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-03-27 12:25:12 +0300

    move styles to assets directory and fix imports

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 // import "./App.css"
-import "./style.css"
+import "../assets/style.css"
 import MainPage from "../features/home/MainPage"
 
 function App() {
