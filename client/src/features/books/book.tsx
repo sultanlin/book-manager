30ddf277300fe95ad52bc3bebf08ea8f1602a18b
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-03-31 17:10:00 +0300

    improve alt tag for img

--- client/src/features/books/book.tsx
+++ client/src/features/books/book.tsx
@@ -3,11 +3,7 @@ import BookMetadata from "@/types/BookProps"
 function Book({ book }: { book: BookMetadata }) {
   return (
     <article className="book">
-      <img
-        src={book.cover}
-        className="cover"
-        alt="everything for dummies book cover"
-      />
+      <img src={book.cover} className="cover" alt="book cover" />
       <p>{book.name}</p>
       <p>{book.author}</p>
     </article>
