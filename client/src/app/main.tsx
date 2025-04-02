a7228866b1bd25c7dffaf331a4f6ea98c39a0536
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-04-02 21:27:29 +0300

    add initial basic routing

--- client/src/app/main.tsx
+++ client/src/app/main.tsx
@@ -2,9 +2,21 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "@/assets/style.css"
 import Search from "@/app/pages/search"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import FirstPage from "@/app/pages/firstpage"
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <FirstPage />,
+  },
+  {
+    path: "/search",
+    element: <Search />,
+  },
+])
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Search />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
