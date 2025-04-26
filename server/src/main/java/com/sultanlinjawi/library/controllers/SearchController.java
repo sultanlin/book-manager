775dd3cb14e1ed09ca72c1559b8785768b232f4d
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-04-26 13:04:23 +0300

    remove cors from controller to global config and refactor (use lombok)

--- server/src/main/java/com/sultanlinjawi/library/controllers/SearchController.java
+++ server/src/main/java/com/sultanlinjawi/library/controllers/SearchController.java
@@ -1,22 +1,19 @@
 package com.sultanlinjawi.library.controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.sultanlinjawi.library.service.SearchService;
+import com.sultanlinjawi.library.services.SearchService;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
 @RestController
+@RequiredArgsConstructor
 public class SearchController {
 
     private final SearchService searchService;
-
-    public SearchController(SearchService searchService) {
-        this.searchService = searchService;
-    }
 
     @GetMapping("/api/v1/search")
     public JsonNode search(
