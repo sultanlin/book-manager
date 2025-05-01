1f92adef4b78105146756544ccfd9ac99c6d2b51
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-05-01 04:39:51 +0300

    add graphqlClient bean and refactor searchServices to use it

---
+++ server/src/main/java/com/sultanlinjawi/library/config/GraphQLClientConfig.java
@@ -0,0 +1,18 @@
+package com.sultanlinjawi.library.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.client.HttpSyncGraphQlClient;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class GraphQLClientConfig {
+
+    @Bean
+    public HttpSyncGraphQlClient graphQlClient() {
+        var restClient = RestClient.create("https://api.hardcover.app/v1/graphql");
+        return HttpSyncGraphQlClient.builder(restClient)
+                .headers((headers) -> headers.add("authorization", System.getenv("authorization")))
+                .build();
+    }
+}
--- server/src/main/java/com/sultanlinjawi/library/services/SearchService.java
+++ server/src/main/java/com/sultanlinjawi/library/services/SearchService.java
@@ -1,34 +1,27 @@
 package com.sultanlinjawi.library.services;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import com.sultanlinjawi.library.dto.SearchResults;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.graphql.client.HttpSyncGraphQlClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClient;
 
 @Service
+@Slf4j
+@AllArgsConstructor
 public class SearchService {
 
     private final HttpSyncGraphQlClient graphQlClient;
-    private final String query;
-
-    public SearchService() {
-        var restClient = RestClient.create("https://api.hardcover.app/v1/graphql");
-        this.graphQlClient =
-                HttpSyncGraphQlClient.builder(restClient)
-                        .headers(
-                                (headers) ->
-                                        headers.add(
-                                                "authorization", System.getenv("authorization")))
-                        .build();
-        this.query =
-                """
-                query bookByName($name: String!, $type: String!) {
-                        search(query: $name, query_type: $type, per_page: 25, page: 1) {
-                                results
-                        }
-                } """;
-    }
+    private final String query =
+            """
+            query bookByName($name: String!, $type: String!) {
+                    search(query: $name, query_type: $type, per_page: 25, page: 1) {
+                            results
+                    }
+            } """;
 
     public JsonNode search(String name, String type) {
         JsonNode project =
