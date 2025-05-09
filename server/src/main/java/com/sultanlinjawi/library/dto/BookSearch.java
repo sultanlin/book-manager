47685002fd303640c97c3a3e4e2ce299386d88f6
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-05-09 12:12:33 +0300

    add better types for postgres integration and builders for easier testing

--- server/src/main/java/com/sultanlinjawi/library/dto/BookSearch.java
+++ server/src/main/java/com/sultanlinjawi/library/dto/BookSearch.java
@@ -2,17 +2,23 @@ package com.sultanlinjawi.library.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import lombok.Builder;
+
 import java.math.BigDecimal;
-import java.net.URL;
+import java.net.URI;
 import java.time.LocalDate;
-import java.util.ArrayList;
+import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Builder
 public record BookSearch(BookSearchResults results) {
-    public record BookSearchResults(ArrayList<BookSearchHit> hits) {
+    @Builder
+    public record BookSearchResults(List<BookSearchHit> hits) {
+        @Builder
         public record BookSearchHit(BookSearchDocument document) {
+            @Builder
             public record BookSearchDocument(
-                    ArrayList<String> author_names,
+                    List<String> author_names,
                     String description,
                     int id,
                     BookSearchImage image,
@@ -23,7 +29,8 @@ public record BookSearch(BookSearchResults results) {
                     String slug,
                     String subtitle,
                     String title) {
-                public record BookSearchImage(URL url) {}
+                @Builder
+                public record BookSearchImage(URI url) {}
             }
         }
     }
