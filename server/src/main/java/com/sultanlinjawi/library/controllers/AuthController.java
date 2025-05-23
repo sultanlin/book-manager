7b39af994703d7a5ee5ba6898d14b8688d6d487e
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-05-23 04:38:50 +0300

    refactor move auth logic to auth service and CREATED responseentity

--- server/src/main/java/com/sultanlinjawi/library/controllers/AuthController.java
+++ server/src/main/java/com/sultanlinjawi/library/controllers/AuthController.java
@@ -3,49 +3,37 @@ package com.sultanlinjawi.library.controllers;
 import com.sultanlinjawi.library.dto.AuthResponse;
 import com.sultanlinjawi.library.dto.LoginRequest;
 import com.sultanlinjawi.library.dto.RegisterRequest;
-import com.sultanlinjawi.library.security.JwtService;
-import com.sultanlinjawi.library.security.UserDetailsImpl;
 import com.sultanlinjawi.library.services.AuthService;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.URI;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
-    private final JwtService jwtService;
 
-    @PostMapping(path = "/login")
+    @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
-        UserDetails userDetails =
-                authService.authenticate(loginRequest.getUsername(), loginRequest.getPassword());
-        String tokenVal = jwtService.generateToken(userDetails);
-        AuthResponse authResponse =
-                AuthResponse.builder().token(tokenVal).username(loginRequest.getUsername()).build();
-        return ResponseEntity.ok(authResponse);
+        var username = loginRequest.getUsername();
+        var password = loginRequest.getPassword();
+        return ResponseEntity.ok(authService.login(username, password));
     }
 
-    @PostMapping(path = "/register")
+    @PostMapping("/register")
     public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest registerRequest) {
-        UserDetailsImpl userDetails = authService.register(registerRequest);
-        String tokenVal = jwtService.generateToken(userDetails);
-        AuthResponse authResponse =
-                AuthResponse.builder()
-                        .token(tokenVal)
-                        .username(registerRequest.getUsername())
-                        .build();
-
-        return ResponseEntity.created(URI.create("/api/v1/users/" + userDetails.getId()))
-                .body(authResponse);
+        var authResponse =
+                authService.register(
+                        registerRequest.getUsername(),
+                        registerRequest.getPassword(),
+                        registerRequest.getPasswordConfirmation());
+        return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
     }
 }
--- server/src/main/java/com/sultanlinjawi/library/services/AuthService.java
+++ server/src/main/java/com/sultanlinjawi/library/services/AuthService.java
@@ -1,12 +1,12 @@
 package com.sultanlinjawi.library.services;
 
-import com.sultanlinjawi.library.dto.RegisterRequest;
+import com.sultanlinjawi.library.dto.AuthResponse;
 import com.sultanlinjawi.library.models.User;
 import com.sultanlinjawi.library.repos.UserRepo;
+import com.sultanlinjawi.library.security.JwtService;
 import com.sultanlinjawi.library.security.UserDetailsImpl;
 
-import jakarta.transaction.Transactional;
-
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,53 +15,61 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class AuthService {
 
     private final AuthenticationManager authenticationManager;
     private final UserDetailsService userDetailsService;
     private final UserRepo userRepo;
+    private final JwtService jwtService;
 
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-    public AuthService(
-            UserDetailsService userDetailsService,
-            AuthenticationManager authenticationManager,
-            UserRepo userRepo) {
-        this.userDetailsService = userDetailsService;
-        this.authenticationManager = authenticationManager;
-        this.userRepo = userRepo;
+    @Transactional
+    public AuthResponse register(String username, String password, String passwordConfirmation) {
+
+        if (!password.equals(passwordConfirmation)) {
+            throw new IllegalArgumentException("Password confirmation failed");
+        }
+
+        var user = createUser(username, password);
+
+        var userDetails = new UserDetailsImpl(user);
+
+        var tokenVal = jwtService.generateToken(userDetails);
+
+        return AuthResponse.builder().token(tokenVal).username(username).build();
     }
 
-    @Transactional
-    public UserDetailsImpl register(RegisterRequest registerRequest) {
-
-        Optional<User> user = userRepo.findByUsername(registerRequest.getUsername());
-
-        if (user.isPresent()) {
-            throw new IllegalArgumentException("Username is taken");
-        }
-
-        if (!registerRequest.getPassword().equals(registerRequest.getPasswordConfirmation())) {
-            throw new IllegalArgumentException("Password does not match password confirmation");
-        }
-
-        var newUser =
-                User.builder()
-                        .username(registerRequest.getUsername())
-                        .password(encoder.encode(registerRequest.getPassword()))
-                        .build();
-        userRepo.save(newUser);
-        return new UserDetailsImpl(newUser);
+    public AuthResponse login(String username, String password) {
+        UserDetails userDetails = authenticate(username, password);
+        String tokenVal = jwtService.generateToken(userDetails);
+        AuthResponse authResponse =
+                AuthResponse.builder().token(tokenVal).username(username).build();
+        return authResponse;
     }
 
     public UserDetails authenticate(String username, String password) {
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(username, password));
         return userDetailsService.loadUserByUsername(username);
+    }
+
+    private User createUser(String username, String password) {
+
+        Optional<User> existingUser = userRepo.findByUsername(username);
+
+        if (existingUser.isPresent()) {
+            throw new IllegalArgumentException("Username taken");
+        }
+
+        var user = User.builder().username(username).password(encoder.encode(password)).build();
+        return userRepo.save(user);
     }
 }
