7781f1b996c02492ee89db72e05bd73776ae5197
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-05-23 03:04:41 +0300

    add tests for create shelf feature

---
+++ server/src/test/java/com/sultanlinjawi/library/services/ShelfServiceTests.java
@@ -0,0 +1,83 @@
+package com.sultanlinjawi.library.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.sultanlinjawi.library.dto.ShelfDto;
+import com.sultanlinjawi.library.models.Shelf;
+import com.sultanlinjawi.library.models.User;
+import com.sultanlinjawi.library.repos.ShelfRepo;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ShelfServiceTests {
+    @Mock private ShelfRepo shelfRepo;
+    @Mock private UserService userService;
+    @Mock private BookService bookService;
+
+    @InjectMocks private ShelfService service;
+
+    @Test
+    @DisplayName("Create shelf, returns shelf after saving (happy ending)")
+    public void ShelfAddedHappyEnding() {
+        var userId = 1;
+        var shelfName = "Test Shelf";
+        var user = User.builder().id(userId).username("tester 1").password("pass").build();
+        var shelf = Shelf.builder().name(shelfName).owner(user).build();
+        var shelf2 = Shelf.builder().name("different name").owner(user).build();
+        user.getShelves().add(shelf2);
+
+        when(userService.getUserById(userId)).thenReturn(user);
+        when(shelfRepo.save(Mockito.any())).thenReturn(shelf);
+
+        var expected = ShelfDto.from(shelf);
+        var actual = service.createShelf(shelfName, userId);
+
+        verify(shelfRepo).save(Mockito.any());
+        Assertions.assertThat(actual).isNotNull();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Create shelf, cannot add a shelf if the user has a shelf with the same name")
+    public void ThrowExceptionIfShelfNameIsTakenForUser() {
+        var userId = 1;
+        var shelfName = "Test Shelf";
+        var user = User.builder().id(userId).username("tester 1").password("pass").build();
+        var shelf = Shelf.builder().name(shelfName).owner(user).build();
+        user.getShelves().add(shelf);
+
+        when(userService.getUserById(userId)).thenReturn(user);
+
+        var expected = "Shelf with this name already exists for this user";
+        var actual =
+                assertThrows(
+                                IllegalArgumentException.class,
+                                () -> service.createShelf(shelfName, userId))
+                        .getMessage();
+
+        assertEquals(expected, actual);
+    }
+
+    // getShelf
+
+    // deleteShelf
+    // Delete shelf that does not belong to user (error thrown and shelf not deleted)
+
+    // updateShelf
+
+    // getBooksFromShelf
+    // addBookToShelf
+    // removeBookFromShelf
+
+}
