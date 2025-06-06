29e58de62afd06a28646971f082a3d1772720882
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-06-07 00:42:03 +0300

    add improved book search bar UI (colors, size, etc)

--- client/src/features/search/search-bar.tsx
+++ client/src/features/search/search-bar.tsx
@@ -5,24 +5,16 @@ import { Form } from "react-router-dom"
 function SearchBar() {
   const [value, setValue] = useState("")
   return (
-    <Form className="search" action="/search">
-      <div className="searchbar">
-        <input
-          type="text"
-          name="search"
-          id="search"
-          placeholder="Search books or authors"
-          onChange={(e) => setValue(e.target.value)}
-          value={value}
-        />
-        <label htmlFor="search">
-          <SearchIcon className="icon" />
-        </label>
-      </div>
-      {/* <select> */}
-      {/*   <option>Book</option> */}
-      {/*   <option>Author</option> */}
-      {/* </select> */}
+    <Form className="searchbar" action="/search">
+      <input
+        type="text"
+        name="search"
+        id="search"
+        placeholder="Search books or authors"
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+      />
+      <SearchIcon className="icon" />
     </Form>
   )
 }
