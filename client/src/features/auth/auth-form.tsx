3ec608b0b2c07b6398e45139fc1436de8f4c55aa
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-06-19 12:15:54 +0300

    replace react router's Fetcher.Form with Form

--- client/src/features/auth/auth-form.tsx
+++ client/src/features/auth/auth-form.tsx
@@ -1,35 +1,42 @@
 import { ReactNode } from "react"
-import { useFetcher, useSearchParams } from "react-router"
+import {
+  Form,
+  useActionData,
+  useNavigation,
+  useSearchParams,
+} from "react-router"
 
 type AuthFormProps = {
   children: ReactNode
   pendingMessage?: string
 }
+
+type actionError = {
+  error: string
+}
+
 function AuthForm({
   children,
   pendingMessage = "Logging in...",
 }: AuthFormProps) {
   const [searchParams] = useSearchParams()
   const redirectTo = searchParams.get("redirectTo")
-  // TODO: Remove fetcher usage
-  // Handle errors and pending without fetcher?!?
-  const fetcher = useFetcher()
+  const data = useActionData() as actionError
 
-  const data = fetcher.data
-
-  const isPending = fetcher.state === "submitting"
+  const navigation = useNavigation()
+  const isPending = navigation.state === "submitting"
 
   return isPending ? (
     <p>{pendingMessage}</p>
   ) : (
-    <fetcher.Form method="post">
+    <Form method="post">
       {data?.error && <p className="error">* {data.error} *</p>}
       {children}
       {redirectTo && (
         <input type="hidden" name="redirectTo" value={redirectTo} />
       )}
       <button type="submit">Login</button>
-    </fetcher.Form>
+    </Form>
   )
 }
 
