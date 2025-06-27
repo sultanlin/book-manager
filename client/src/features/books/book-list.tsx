7ff3c5bd6a4e353eb58d69a8407b06f35302ab09
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-06-28 01:41:36 +0300

    add book key to book-list and make books clickable (link)

--- client/src/features/books/book-list.tsx
+++ client/src/features/books/book-list.tsx
@@ -1,10 +1,12 @@
 import { Book } from "@/types/api"
-import { ReactNode } from "react"
 import BookView from "./book-view"
 
-function BookList({ booksList }: { booksList: Book[] }): ReactNode {
-  // FIX: Add keys to list
-  return booksList.map((b) => <BookView book={b} />)
+type BookListProps = {
+  booksList: Book[]
+}
+
+function BookList({ booksList }: BookListProps) {
+  return booksList.map((book) => <BookView book={book} key={book.id} />)
 }
 
 export default BookList
--- client/src/features/books/book-view.tsx
+++ client/src/features/books/book-view.tsx
@@ -1,11 +1,14 @@
 import { Book } from "@/types/api"
+import { Link } from "react-router"
 
 function BookView({ book }: { book: Book }) {
   return (
     <article className="book">
-      <img src={book.cover} className="cover" alt="book cover" />
-      <p className="name">{book.title}</p>
-      <p className="author">{book.author}</p>
+      <Link to={`/books/${book.id}`}>
+        <img src={book.cover} className="cover" alt="book cover" />
+        <p className="name">{book.title}</p>
+        <p className="author">{book.author}</p>
+      </Link>
     </article>
   )
 }
