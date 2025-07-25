1bbf793591c157234e535e0680579a14766357d3
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-07-26 00:05:39 +0300

    add error boundary for app root

--- client/src/app/root.tsx
+++ client/src/app/root.tsx
@@ -1,37 +1,15 @@
-import {
-  isRouteErrorResponse,
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "react-router"
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router"
 import "@/assets/style.css"
 
-import { Route } from "./+types/root"
-
-export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-  if (isRouteErrorResponse(error)) {
-    return (
-      <>
-        <h1>
-          {error.status} {error.statusText}
-        </h1>
-        <p>{error.data}</p>
-      </>
-    )
-  } else if (error instanceof Error) {
-    return (
-      <div>
-        <h1>Error</h1>
-        <p>{error.message}</p>
-        <p>The stack trace is:</p>
-        <pre>{error.stack}</pre>
-      </div>
-    )
-  } else {
-    return <h1>Unknown Error</h1>
-  }
+export function ErrorBoundary() {
+  return (
+    <div className="boundary" role="alert">
+      <h2>Ooops, something went wrong :( </h2>
+      <button onClick={() => window.location.assign(window.location.origin)}>
+        Refresh
+      </button>
+    </div>
+  )
 }
 
 export function Layout({ children }: { children: React.ReactNode }) {
