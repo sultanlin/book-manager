275289a137e916ab68de08510c9604a490deaaa7
Sultan Linjawi <skorpion219@hotmail.co.uk>
2025-07-27 23:49:07 +0300

    change logo redirect to / home instead of /search

--- client/src/features/header/logo.tsx
+++ client/src/features/header/logo.tsx
@@ -2,7 +2,7 @@ import { LocalLibraryRounded } from "@mui/icons-material"
 import { Link } from "react-router"
 function Logo() {
   return (
-    <Link to={"/search"} className="logo">
+    <Link to={"/"} className="logo">
       <LocalLibraryRounded className="icon" />
       <h1>Library</h1>
     </Link>
